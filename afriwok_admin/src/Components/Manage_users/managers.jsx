4261d59fe06aed9cc8bb88f2fcaa16f02104b866
EbenezerNwolisa100 <ebenezernwolisa100@gmail.com>
2025-01-13 23:51:17 +0100

    managers updated

--- afriwok_admin/src/Components/Manage_users/managers.jsx
+++ afriwok_admin/src/Components/Manage_users/managers.jsx
@@ -311,7 +311,7 @@ export default function ManageUsers() {
   };
 
   return (
-    <div className="max-w-[1400px] mx-auto bg-[#F9F9F9]">
+    <div className="w-full mx-auto bg-[#F9F9F9]">
       {/* Tabs */}
       <div className="border-b mb-6 -mx-2 md:mx-0">
         <div className="flex overflow-x-auto scrollbar-hide">
